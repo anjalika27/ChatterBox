bc67c0fc0c46c5c0e5ad9002f56f713f91b2945a
Anjalika <anjalika.agarwal@gmail.com>
2025-02-23 14:25:10 +0530

    user profile component

--- frontend/src/components/ChatRoom.jsx
+++ frontend/src/components/ChatRoom.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import ChatUsersList from './ChatUsersList.jsx'
 import TextInput from './TextInput.jsx'
+import Navbar from './Navbar.jsx'
 
 export default function ChatRoom() {
     return (
-        <div className='container' style={{ display: 'flex', padding: 0, height: '100%' }}>
-            <div style={{ width: '30%', height: '100%' }}>
-                <ChatUsersList />
-            </div>
-            <div style={{ width: '70%', height: '100%', display: 'flex', flexDirection: 'column' }}>
-                <div className="messageList" style={{height:'95%'}}>
-
+        <>
+            <Navbar />
+            <div className='container' style={{ display: 'flex', padding: 0, height: '100%' }}>
+                <div style={{ width: '30%', height: '100%' }}>
+                    <ChatUsersList />
                 </div>
-                <TextInput />
+                <div style={{ width: '70%', height: '93%', display: 'flex', flexDirection: 'column' }}>
+                    <div className="messageList" style={{ height: '95%' }}>
+                        all messages
+                    </div>
+                    <TextInput />
+                </div>
             </div>
-        </div>
+
+        </>
     )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+export default function Navbar() {
+    const { user, isAuthenticated } = useAuth0()
+    return (
+        <>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', padding: '4px 10px' }}>
+                <div>sljerb</div>
+                <button class="btn btn-outline-info" data-bs-toggle="modal" data-bs-target="#staticBackdrop" style={{padding:0}}>
+                    <img src={isAuthenticated ? user.picture : "user.png"} alt="user" style={{ height: '28px', width: '28px' }} />
+                </button>
+            </div>
+
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Profile</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p><strong>Name: </strong>{isAuthenticated ? user.name : ""}</p>
+                            <p><strong>Email: </strong>{isAuthenticated ? user.email : ""}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+
+}
